5c4cc51e95821199eade1a7c83d956cb9a6bdc08
raviolisgithub <aaravrajup@gmail.com>
2023-09-24 19:09:46 +0400

    printing text

--- src/main.asm
+++ src/main.asm
@@ -2,7 +2,35 @@
 infinite_loop:
     jmp loop
 
+mov ah, 0x0e ; Teletype mode
 
+; prints text
+mov al, 'R'
+int 0x10
+
+mov al, 'A'
+int 0x10
+
+mov al, 'V'
+int 0x10
+
+mov al, 'I'
+int 0x10
+
+mov al, 'O'
+int 0x10
+
+mov al, 'L'
+int 0x10
+
+mov al, 'I'
+int 0x10
+
+mov al, 'O'
+int 0x10
+
+mov al, 'S'
+int 0x10
 
 ; Fills the img
 times 510-($-$$) db 0
